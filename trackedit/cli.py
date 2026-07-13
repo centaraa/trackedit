b24d6a65ce4f12db56e9eb552f87133cac2daf9c
Bernhard Magerl <bernhard.magerl@uzh.ch>
2026-07-13 17:10:34 +0200

    explainatory comment added

--- trackedit/cli.py
+++ trackedit/cli.py
@@ -1,5 +1,5 @@
 """Command line interface for trackedit."""
-# Usage: pixi run python cli.py convert tiff_to_zarr path/in -o path/out
+# Usage: pixi run python cli.py convert tiff-to-zarr path/in -o path/out
 
 from pathlib import Path
 
